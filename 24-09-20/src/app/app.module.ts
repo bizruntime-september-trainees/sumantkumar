6c88703e63dcfd6ed977f96bc69f21772f95c719
sumant kumar <sumantkumar735568@gmail.com>
2020-09-26 07:20:27 +0530

    material ui implemenation

---
+++ 24-09-20/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { MaterialModule } from './material/material.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { GridListComponent } from './grid-list/grid-list.component';
+import { StepperComponent } from './stepper/stepper.component';
+import { TabsComponent } from './tabs/tabs.component';
+import { ExpansionPanelComponent } from './expansion-panel/expansion-panel.component';
+import { TableComponent } from './table/table.component';
+import { FormComponent } from './form/form.component';
+import { SlideBarComponent } from './slide-bar/slide-bar.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { ReactiveFormsModule } from '@angular/forms';
+@NgModule({
+  declarations: [
+    AppComponent,
+    GridListComponent,
+    StepperComponent,
+    TabsComponent,
+    ExpansionPanelComponent,
+    TableComponent,
+    FormComponent,
+    SlideBarComponent,
+    HeaderBarComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
